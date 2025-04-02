ed19474c4b6cae788ef9f432a9a48e32178b66e4
Mikhail S. Pobolovets <styx.mp@gmail.com>
2025-04-02 14:28:50 +0200

    Adopt setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ Lpf2Hub myHub;
 byte port = (byte)PoweredUpHubPort::A;
 
 //Pin declaration
-#define BTN_MUSIC 25
-#define BTN_LICHT 26
-#define BTN_WASSER 27
-#define BTN_STOP 14
-#define PTI_SPEED 15
+#define BTN_MUSIC 27
+#define BTN_LIGHT 26
+#define BTN_WATER 14
+#define BTN_STOP 25
+#define PTI_SPEED 12
 
 Bounce pbMusic = Bounce();
 Bounce pbLight = Bounce();
@@ -26,17 +26,23 @@ void handlePoti()
 {
   int ptiSpeed = analogRead(PTI_SPEED);
   gLastStatePtiSpeed = ptiSpeed;
-  //Serial.println(ptiSpeed);
+  // Serial.println(ptiSpeed);
   int speed = 0;
-  if ( ptiSpeed > 1100) speed = 64;     //Fast foreward
-  else if (ptiSpeed > 800) speed = 32;  //normal forweard
-  else if (ptiSpeed > 600) speed = 16;  //slow forefard (might not work on low battery) 
-  else if (ptiSpeed > 400) speed = 0;   //stop
-  else if (ptiSpeed > 200) speed = -32; //slow backward
-  else speed = -64;                     //fast foreward
+  // if ( ptiSpeed > 2600) speed = 64;      // fast foreward
+  // else if (ptiSpeed > 2400) speed = 32;  // normal forweard
+  // else if (ptiSpeed > 2100) speed = 16;  // slow forefard (might not work on low battery)
+  // else if (ptiSpeed < 1800) speed = -16; // slow backward
+  // else if (ptiSpeed < 1400) speed = -32; // fast backward
+
+  if (ptiSpeed < 1200) speed = 64;
+  else if (ptiSpeed < 1400) speed = 32;
+  else if (ptiSpeed < 1650) speed = 16;
+  else if (ptiSpeed > 2100) speed = -32;
+  else if (ptiSpeed > 1850) speed = -16;
+  else speed = 0;                        // stop
 
   if(speed != gSpeed)
-  { 
+  {
     if(gSpeed == 0 && speed > 0)
     {
         myHub.playSound((byte)DuploTrainBaseSound::STATION_DEPARTURE);
@@ -51,19 +57,19 @@ void handlePoti()
 Color getNextColor()
 {
     if(gColor == 255)
-    { 
+    {
         gColor = 0;
         return (Color)0;
     }
-       
+
     gColor++;
-    
+
     if(gColor == NUM_COLORS)
-    {   
+    {
         gColor =255;
     }
     return (Color) gColor;
-} 
+}
 
 void handleButtons()
 {
@@ -88,7 +94,7 @@ void handleButtons()
     if(pbWater.fell())
     {
       myHub.playSound((byte)DuploTrainBaseSound::WATER_REFILL);
-      delay(100);  
+      delay(100);
     }
   }
   if(pbStop.update())
@@ -106,8 +112,8 @@ void setup() {
   Serial.begin(115200);
   //define Pin Modes
   pbMusic.attach(BTN_MUSIC, INPUT_PULLUP);
-  pbLight.attach(BTN_LICHT, INPUT_PULLUP);
-  pbWater.attach(BTN_WASSER, INPUT_PULLUP);
+  pbLight.attach(BTN_LIGHT, INPUT_PULLUP);
+  pbWater.attach(BTN_WATER, INPUT_PULLUP);
   pbStop.attach(BTN_STOP, INPUT_PULLUP);
 
   myHub.init();
@@ -126,6 +132,6 @@ void loop() {
     if (myHub.isConnected()) {
         handleButtons();
         handlePoti();
-    } 
+    }
     delay(50);
 }
